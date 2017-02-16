55d81ead1859fe4d171f8958bb2229b7a3981186
rong.ye <yerongsc@163.com>
2017-02-16 21:13:05 +0800

    add ui lua

---
+++ Assets/Test/UI/UIRootHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace XLuaTest
+{
+	public class UIRootHandler : LuaBehaviour
+	{
+		private Dictionary<string,UIComponent> _uiComponents = new Dictionary<string, UIComponent>();
+		public Dictionary<string,UIComponent> uiComponents
+		{
+			get{return _uiComponents;}
+		}
+
+		public void Resgister(UIComponent uiComponent)
+		{			
+			if(uiComponent != null)
+			{
+				_uiComponents[uiComponent.name] = uiComponent;
+			}
+		}
+
+		/*
+		void Start()
+		{
+			Dictionary<string,UIComponent>.Enumerator iter = _uiComponents.GetEnumerator();
+			while(iter.MoveNext())
+			{
+				
+			}
+			base.Start();
+		}*/
+	}
+}

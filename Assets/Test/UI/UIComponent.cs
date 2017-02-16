9c9d69ce034e11c92f9caa31575fd166e6e15a44
rong.ye <yerongsc@163.com>
2017-02-16 21:13:05 +0800

    add ui lua

---
+++ Assets/Test/UI/UIComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+namespace XLuaTest
+{
+	public class UIComponent : LuaBehaviour 
+	{
+		public UIRootHandler rootHanlder;
+
+		void Awake()
+		{
+			if(rootHanlder != null)
+			{
+				rootHanlder.Resgister(this);
+			}
+
+		}			
+	}
+}

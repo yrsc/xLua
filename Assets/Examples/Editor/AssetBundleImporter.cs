fee9dcb67cd01bf13262e3cb81a9cfbde362ecee
rong.ye <yerongsc@163.com>
2017-03-21 23:17:19 +0800

    add assetbundle

---
+++ Assets/Examples/Editor/AssetBundleImporter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AssetBundleImporter : AssetPostprocessor {
+
+	static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)   
+	{  
+		//assetbundle can not contain .cs only
+		foreach (var str in importedAssets)  
+		{  
+			if(!str.EndsWith(".cs"))  
+			{  
+				AssetImporter importer = AssetImporter.GetAtPath(str);  
+				importer.assetBundleName = str;  
+			}  
+		}  
+
+		foreach (var str in movedAssets)  
+		{  
+			if(!str.EndsWith(".cs"))  
+			{  
+				AssetImporter importer = AssetImporter.GetAtPath(str);  
+				importer.assetBundleName = str;  
+			}  
+		}  
+	}  
+
+
+}
